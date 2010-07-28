5d198ff632d68694258eaf96e72ec1f2f06ba4ae
Brian Cassidy <bricas@cpan.org>
2010-07-28 14:20:44 +0000

    Import of BRICAS/Games-NES-ROM-0.06 from CPAN.
    gitpan-cpan-distribution: Games-NES-ROM
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         BRICAS/Games-NES-ROM-0.06.tar.gz
    gitpan-cpan-author:       BRICAS
    gitpan-cpan-maturity:     released

--- lib/Games/NES/ROM.pm
+++ lib/Games/NES/ROM.pm
@@ -1,192 +1,86 @@
 package Games::NES::ROM;
 
-=head1 NAME
-
-Games::NES::ROM - View information about an NES game from a ROM file
-
-=head1 SYNOPSIS
-
-    use Games::NES::ROM;
-    
-    # Read in the ROM
-    my $rom = Games::NES::ROM->new( 'file.nes' );
-
-    # Access the details
-    print 'PRG Banks: ', $rom->PRG_count, "\n";
-    print 'CHR Banks: ', $rom->CHR_count, "\n";
-    # etc...
-    
-    # View the CRC checksum
-    print 'CRC: ', $rom->CRC;
-
-=head1 DESCRIPTION
-
-This module loads the details of an NES rom file. An NES
-ROM file is layed out as follows:
-
-    +-----------+---------------+---------+---------+ Header
-    | NES\0x01a | [PC] [CC] X X | X X X X | X X X X | 16 Bytes
-    +-----------+---------------+---------+---------+
-    |                                               |
-    |          PRG Banks (PC * 16384 Bytes)         |
-    |                                               |
-    +-----------------------------------------------+
-    |                                               |
-    |          CHR Banks (CC * 8192 Bytes)          |
-    |                                               |
-    +-----------------------------------------------+
-    |                                               |
-    |          Title (128 Bytes - Optional)         |
-    |                                               |
-    +-----------------------------------------------+
-
-=head1 INSTALLATION
-
-    perl Makefile.PL
-    make
-    make test
-    make install
-
-=cut
-
-use base qw( Class::Accessor::Fast );
-
-use strict;
-use warnings;
-
-use constant HEADER_SIZE   => 16;
-use constant TITLE_SIZE    => 128;
-use constant TRAINER_SIZE  => 512;
-use constant PRG_BANK_SIZE => 16384;
-use constant CHR_BANK_SIZE => 8192;
-
+use Moose;
+use Try::Tiny;
 use FileHandle;
-use Digest::CRC;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
-my $header_template = 'A4 C*';
-my @header_fields   = qw( identifier PRG_count CHR_count mapper );
+has 'filename' => ( is => 'rw' );
 
-__PACKAGE__->mk_accessors( @header_fields, qw( title has_trainer trainer horizontal_mirroring vertical_mirroring VRAM SRAM PRG_banks CHR_banks CRC ) );
+has 'filehandle' => ( is => 'rw', isa => 'FileHandle', lazy_build => 1 );
 
-=head1 METHODS
+has 'id' => ( is => 'ro' );
 
-=head2 new( [$filename] )
+has 'title' => ( is => 'rw' );
 
-Creates a new instance of the module. An optional filename
-argument may be passed to immediately load the data.
+has 'prg_banks' => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
 
-=cut
+has 'chr_banks' => ( is => 'rw', isa => 'ArrayRef', default => sub { [] } );
 
-sub new {
-    my $class = shift;
-    my $file  = shift;
-    my $self  = {};
+has 'has_sram' => ( is => 'rw', isa => 'Bool', default => 0 );
 
-    bless $self, $class;
+has 'mirroring' => ( is => 'rw', isa => 'Int', default => 0 );
 
-    $self->load( $file ) if $file;
+has 'mapper' => ( is => 'rw' );
 
-    return $self;
+sub _build_filehandle {
+    my $self = shift;
+    my $file = $self->filename;
+    my $fh   = FileHandle->new( $file, '<' );
+
+    die "Unable to open ${file}: $!" unless defined $fh;
+
+    binmode( $fh );
+    return $fh;
 }
 
-=head2 load( $file )
-
-Reads the ROM structure into memory.
-
-=cut
+__PACKAGE__->meta->make_immutable;
 
 sub load {
-    my $self = shift;
-    my $file = shift;
+    my( $class, $file ) = @_;
 
-    my $rom = FileHandle->new( $file, '<' );
+    for( qw( INES UNIF ) ) {
+        my $class = 'Games::NES::ROM::Format::' . $_;
+        Class::MOP::load_class( $class );
 
-    die "Unable to open ${file}: $!" unless defined $rom;
+        my $rom = try {
+            $class->new( filename => $file );
+        };
 
-    binmode( $rom );
-
-    my $header;
-    $rom->read( $header, HEADER_SIZE );
-
-    die 'Not an NES rom' unless $header =~ /^NES/;
-
-    my @values = unpack( $header_template, $header );
-
-    for( 0..2 ) {
-        $self->set( $header_fields[ $_ ] => $values[ $_ ] );
+        return $rom if $rom;
     }
 
-    $self->horizontal_mirroring( $values[ 3 ] & 1 ^ 1 );
-    $self->vertical_mirroring( $values[ 3 ] & 1 );
-    $self->SRAM( $values[ 3 ] & 2 );
-    $self->has_trainer( $values[ 3 ] & 4 );
-    $self->VRAM( $values[ 3 ] & 8 );
-
-    my $prg_count = $self->PRG_count;
-    my $chr_count = $self->CHR_count;
-
-    my $mapper = ( $values[ 3 ] & 240 ) >> 4;
-    $mapper   |= ( $values[ 4 ] & 240 );
-
-    if( $mapper != 0 and ( $prg_count == 2 or $prg_count == 1 ) and $chr_count == 1 ) {
-        $mapper = 0;
-    }
-
-    $self->mapper( $mapper );
-
-    if( $self->has_trainer ) {
-        my $trainer;
-        $rom->read( $trainer, TRAINER_SIZE );
-        $self->trainer( $trainer );
-    }
-
-    my @prg_banks = map {
-        my $data;
-        $rom->read( $data, PRG_BANK_SIZE );
-        $data;
-    } 1..$prg_count;
-
-    my @chr_banks = map {
-        my $data;
-        $rom->read( $data, CHR_BANK_SIZE );
-        $data;
-    } 1..$chr_count;
-
-    $self->PRG_banks( \@prg_banks );
-    $self->CHR_banks( \@chr_banks );
-
-    my $title;
-    if($rom->read( $title, TITLE_SIZE ) == 128 ) {
-        $self->title( $title );
-    }
-
-    $rom->seek( HEADER_SIZE, 0 );
-
-    my $ctx = Digest::CRC->new( type=> 'crc32' );
-    $ctx->addfile( $rom );
-    $self->CRC( $ctx->hexdigest );
-
-    $rom->close;
+    die "${file} is not an NES rom";
 }
 
-=head2 sprite( $chr_bank, $index )
+sub prg_count {
+    return scalar @{ shift->prg_banks };
+}
 
-Returns the raw (composite) sprite in the specified 
-CHR bank at the specified array index.
+sub chr_count {
+    return scalar @{ shift->chr_banks };
+}
 
-=cut
+sub sha1 {
+    my $self = shift;
+    require Digest::SHA1;
+    return Digest::SHA1::sha1_hex( @{ $self->prg_banks }, @{ $self->chr_banks } );
+}
+
+sub crc {
+    my $self = shift;
+    require Digest::CRC;
+    return Digest::CRC::crc32_hex( join( '', @{ $self->prg_banks }, @{ $self->chr_banks } ) );
+}
 
 sub sprite {
-    my $self   = shift;
-    my $chr    = shift;
-    my $offset = shift;
+    my( $self, $chr, $offset ) = @_;
 
-    die 'invalid CHR bank' if $chr > $self->CHR_count - 1 or $chr < 0;
+    die 'invalid CHR bank' if $chr > $self->chr_count - 1 or $chr < 0;
     die 'invalid sprite index' if $offset > 512 or $offset < 0;
     
-    my $bank      = $self->CHR_banks->[ $chr ];
+    my $bank      = $self->chr_banks->[ $chr ];
     my $start     = 16 * $offset;
     my @channel_a = unpack( 'C*', substr( $bank, $start, 8 ) );
     my @channel_b = unpack( 'C*', substr( $bank, $start + 8, 8 ) );
@@ -209,47 +103,125 @@ sub _combine_bits {
     return ( ( $chan_a >> $offset ) & 1 ) | ( ( ( $chan_b >> $offset ) & 1 ) << 1 );
 }
 
-=head1 ACCESSORS
+1;
 
-The following accessors are available:
+__END__
+
+=head1 NAME
+
+Games::NES::ROM - View information about an NES game from a ROM file
+
+=head1 SYNOPSIS
+
+    use Games::NES::ROM;
+    
+    # Read in the ROM without having to know its format
+    my $rom = Games::NES::ROM->load( 'file.nes' );
+
+    # Specifically read in an iNES file
+    $rom = Games::NES::ROM::INES->new( filename => 'file.nes' );
+
+    # Access the details
+    print 'PRG Banks: ', $rom->prg_count, "\n";
+    print 'CHR Banks: ', $rom->prg_count, "\n";
+    # etc...
+    
+    # View the SHA-1 & CRC checksums
+    print 'SHA1: ', $rom->sha1, "\n";
+    print ' CRC: ', $rom->crc, "\n";
+
+=head1 DESCRIPTION
+
+This module loads the details of an NES rom file. It is primarily meant to be
+used a base class for more specific file formats. Those formats include:
 
 =over 4
 
-=item * identifier - sould be "NES\0x01a"
+=item * L<Universal NES Image File (UNIF)|Games::NES::ROM::Format::UNIF>
 
-=item * PRG_count - number of PRG banks
-
-=item * CHR_count - number of CHR banks
-
-=item * PRG_banks - arrayref of PRG data
-
-=item * CHR_banks - array ref of CHR data
-
-=item * mapper - the mapper number
-
-=item * has_trainer - is there trainer data?
-
-=item * trainer - the trianer data
-
-=item * horizontal_mirroring - does it use horizontal mirroring?
-
-=item * vertical_mirroring - does it use veritcal mirroring?
-
-=item * VRAM - uses VRAM?
-
-=item * SRAM - uses SRAM?
-
-=item * CRC - CRC checksum
-
-=item * title - game title if available 
+=item * L<iNES|Games::NES::ROM::Format::INES>
 
 =back
+
+=head1 INSTALLATION
+
+    perl Makefile.PL
+    make
+    make test
+    make install
+
+=head1 METHODS
+
+=head2 load( $filename )
+
+Attemps to read the ROM structure into memory using all available file
+formats until success.
+
+=head2 prg_count( )
+
+Returns the number of PRG banks for this ROM.
+
+=head2 chr_count( )
+
+Returns the number of CHR banks for this ROM.
+
+=head2 sha1( )
+
+Returns the SHA-1 checksum for the PRG and CHR data.
+
+=head2 crc( )
+
+Returns the CRC checksum for the PRG and CHR data.
+
+=head2 sprite( $chr_bank, $index )
+
+Returns the raw (composite) sprite in the specified 
+CHR bank at the specified array index.
+
+=head1 BASE ATTRIBUTES
+
+The following base attributes are available for all file formats:
+
+=over 4
+
+=item * filename - The filename from which data was loaded
+
+=item * id - A string found at the beginning of a file to identify the file format
+
+=item * title - The game's title, if available 
+
+=item * prg_banks - An arrayref of PRG bank data
+
+=item * chr_banks - An arrayref of CHR bank data
+
+=item * has_sram - Boolean value to determine if the ROM is battery backed
+
+=item * mapper - A value indicating what memory mapper to use with the ROM
+
+=item * mirroring - A value indicating what time of mirroring is used in the ROM
+
+=back
+
+=head2 NOTES
+
+=over 4
+
+=item * mapper - iNES uses integer IDs, UNIF uses string IDs
+
+=item * mirroring - An integer ID, using the UNIF list as a basis
+
+=back
+
+Each file format will have an extended set of attributes specific to its data
+structure. Please consult their documentation for more information.
 
 =head1 SEE ALSO
 
 =over 4
 
-=item * http://www.sadistech.com/nesromtool/romdoc.html
+=item * L<Games::NES::ROM::Format::INES>
+
+=item * L<Games::NES::ROM::Format::UNIF>
 
 =back
 
@@ -259,11 +231,10 @@ Brian Cassidy E<lt>bricas@cpan.orgE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 by Brian Cassidy
+Copyright 2007-2010 by Brian Cassidy
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself. 
 
 =cut
 
-1;
--- t/03-sprite.t
+++ t/03-sprite.t
@@ -1,8 +1,11 @@
+use strict;
+use warnings;
+
 use Test::More tests => 13;
 
 use_ok( 'Games::NES::ROM' );
 
-my $rom = Games::NES::ROM->new( 't/roms/test.nes' );
+my $rom = Games::NES::ROM->load( 't/roms/test.nes' );
 isa_ok( $rom, 'Games::NES::ROM' );
 
 my $sprite = $rom->sprite( 0, 0 );
